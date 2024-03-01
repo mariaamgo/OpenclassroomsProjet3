ab26b293d448a31b2894ff08666833ebe4b26571
Maria Amigo <maria.amigo.07@gmail.com>
2024-03-01 08:38:08 +0100

    modification des fichiers login.js et login.html

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,24 +1,25 @@
 //function login(){
     const formLogin = document.querySelector("#login form")
-    const email = "sophie.bluel@test.tld"
-    const password = "S0phie"
+    const email = "sophie.bluel@test.tld";
+    const password = "S0phie";
     formLogin.addEventListener("submit", function(event){
-        event.preventDefault()
+        event.preventDefault();
         const log = {
             email: event.target.querySelector("[name=email]").value,
             password: event.target.querySelector("[name=password]").value
         };
 
         if(log.email === email && log.password === password){
-            console.log("ok")
+            document.location.href="../index.html";
         }else{
-            console.log("raté")
+            const error = document.querySelector("#login p");
+            error.innerText = "L'e-mail ou le mot de passe est erroné";
         }
         // Appel de la fonction fetch avec toutes les informations nécessaires
         fetch("http://localhost:5678/api/users/login", {
             method: "POST",
             headers: {"Content-Type" : "application/json"},
             body: JSON.stringify(log)
-        })
-    })
+        });
+    });
 //}
