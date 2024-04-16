a5426b39175f80c49b7033d0d60fa9ec25ccbb19
Maria Amigo <maria.amigo.07@gmail.com>
2024-04-16 07:55:26 +0200

    suppression de export, ajout de la fonction fetchLogin

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,33 +1,34 @@
-export function login(){
+function login(){
     const formLogin = document.querySelector("form");
-    formLogin.addEventListener("submit", function(event){
-        event.preventDefault();
+    formLogin.addEventListener("submit", (event) => fetchLogin(event));
+}
+
+function fetchLogin(event){
+    event.preventDefault();
         const log = {
             email: event.target.querySelector("[name=email]").value,
             password: event.target.querySelector("[name=password]").value
         };
-        console.log(log)
-            fetch("http://localhost:5678/api/users/login", {
-                method: "POST",
-                headers: {"Content-Type" : "application/json"},
-                body: JSON.stringify(log)
-            })
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                if(data.token === undefined){
-                    alert("Erreur dans l’identifiant ou le mot de passe");
-                }else{
-                    //récupération du token et stockage du token
-                    const token = data.token;
-                    sessionStorage.setItem("response", token);
-                    //console.log(localStorage.getItem("response"));
-                    window.location.href='../index.html';
-                }
-            })
-
-    });
+        
+        fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify(log)
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            if(data.token === undefined){
+                alert("Erreur dans l’identifiant ou le mot de passe");
+            }else{
+                //récupération du token et stockage du token
+                const token = data.token;
+                sessionStorage.setItem("response", token);
+                //console.log(localStorage.getItem("response"));
+                window.location.href="../index.html";
+            }
+        })
 }
 login();
 
