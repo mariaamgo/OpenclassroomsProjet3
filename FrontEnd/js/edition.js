2e1408f12981e92b38e700a034c8aa77758c4e82
Maria Amigo <maria.amigo.07@gmail.com>
2024-04-22 16:58:23 +0200

    ajout des div card-edition et de la div portfolio-title

--- FrontEnd/js/edition.js
+++ FrontEnd/js/edition.js
@@ -14,19 +14,27 @@ export function edition(works){
 
 //fonction pour changer l'apparence de index.html en mode edition
 function modeEdition(){
+    const header = document.querySelector("header");
+    const cardEdition = `<div class="card-edition">
+                            <div class = "edition">
+                                <i class="fa-regular fa-pen-to-square"></i>
+                                <p>Mode édition</p>
+                            </div>
+                        </div>`;
+    header.insertAdjacentHTML('beforebegin', cardEdition);
+
+    const title_portfolio = document.querySelector("#portfolio h2");
+    const edition = `<div class= "edition">
+                        <i class="fa-regular fa-pen-to-square"></i>
+                        <a href="#modal" class="js-modal">modifier</a>
+                    </div>`;
+    title_portfolio.insertAdjacentHTML('afterend', edition);
+    title_portfolio.style = "margin-bottom: 100px";
+
     const liLogin = document.querySelector("#liLogin");
     liLogin.innerText = "logout";
-    const cardEdition = document.querySelector(".card-edition");
-        cardEdition.style = "display: block";
-        const edition = document.querySelectorAll(".edition");
-        for(let i=0; i< edition.length; i++){
-            edition[i].style = "display : flex";
-        }
 
-        const filter = document.querySelector(".filter");
-        filter.style = "display : none";
-
-        const portfolioTitle = document.querySelector(".portfolio-title");
-        portfolioTitle.style = "margin-bottom: 100px";
+    const filter = document.querySelector(".filter");
+    filter.style = "display : none";
 }
 
