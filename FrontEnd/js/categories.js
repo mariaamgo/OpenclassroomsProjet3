7e46f5a9634b5336ed1b801cdf41437374141995
Maria Amigo <maria.amigo.07@gmail.com>
2024-04-28 10:57:08 +0200

    ajout de la fonction fetchResponseError et catchError

--- FrontEnd/js/categories.js
+++ FrontEnd/js/categories.js
@@ -1,10 +1,15 @@
+import { fetchResponseError, catchError } from "./error.js";
+
 export async function getCategories(){
     try {
         const response = await fetch("http://localhost:5678/api/categories");
+
+        fetchResponseError(response, "Une erreur s'est produite lors de la récupération des catégories");
+        
         let categories = await response.json();
         categories = [...new Set(categories)]; // Traiter les catégories pour éliminer les doublons
         return categories;
     } catch (error) {
-        throw new Error("Une erreur s'est produite lors de la récupération des catégories :", error);
+        catchError(error);
     }
 }
