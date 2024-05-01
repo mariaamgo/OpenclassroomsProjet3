7b0c15e77291c7f2d893981e14a15472f26c0b36
Maria Amigo <maria.amigo.07@gmail.com>
2024-05-01 13:27:01 +0200

    ajout de commentaires

--- FrontEnd/js/categories.js
+++ FrontEnd/js/categories.js
@@ -3,13 +3,14 @@ import { fetchResponseError, catchError } from "./error.js";
 export async function getCategories(){
     try {
         const response = await fetch("http://localhost:5678/api/categories");
-
+        //fonction qui vient de la page error.js pour créer une nouvelle erreur
         fetchResponseError(response, "Une erreur s'est produite lors de la récupération des catégories");
         
         let categories = await response.json();
         categories = [...new Set(categories)]; // Traiter les catégories pour éliminer les doublons
         return categories;
     } catch (error) {
+        //fonction qui vient de la page error.js pour afficher l'erreur en console et en alert
         catchError(error);
     }
 }
